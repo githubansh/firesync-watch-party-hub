babb8bef80818ada544a9628a3cae864574fd906
Bhavya <anim8tor.ing@gmail.com>
2025-06-21 15:12:51 +0530

    changed color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
-import Mobile from "./pages/Mobile";
-import FireTV from "./pages/FireTV";
+import Mobile from './pages/Mobile';
+import FireTV from './pages/FireTV';
 import {FireTVApp1} from "@/components/firetv/FireTVApp1";
 import NotFound from "./pages/NotFound";
 
